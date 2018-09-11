3b00b6d006058279a7d036036ba2b4fc0406b8a0
14030609 <mike.angyz@gmail.com>
2018-09-10 20:36:29 -0500

    changes ok

--- src/ai/rbs/Main.java
+++ src/ai/rbs/Main.java
@@ -3,8 +3,8 @@ package ai.rbs;
 public class Main {
 
     public static void main(String[] args) {
-        /*Preload p = new Preload("RULES.txt");
-        p.createBinaryFile();*/
+        Preload p = new Preload("RULES.txt");
+        p.createBinaryFile();
         UI ui = new UI();
         ui.createUI();
 
