791691dd564a74dd1421fce7216037d99916bc0e
14030609 <mike.angyz@gmail.com>
2018-09-10 19:16:17 -0500

    cambios

---
+++ src/ai/rbs/GUI.java
@@ -0,0 +1,17 @@
+package ai.rbs;
+
+import javax.swing.*;
+
+public class GUI {
+    private JTabbedPane tabbedPane1;
+    private JTextArea textArea1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton finalizarButton;
+    private JButton analizarButton;
+    private JButton insertarButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
